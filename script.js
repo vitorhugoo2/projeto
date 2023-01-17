04e76576211f74e9353330e394c515e3a04a155b
Vitor Hugo <vitor.newmetal2@gmail.com>
2023-01-16 21:16:18 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+
+    // pegar a tag img
+
+    const img = document.querySelector("#profile img")
+
+    // Substituir a imagem 
+    
+    if (html.classList.contains("light")) {
+        img.setAttribute("src","./assets/avatar-light.png")
+     }
+    else {
+        
+        img.setAttribute("src", "./assets/avatar.png")
+        
+    
+}
+    
+
+    }
+
+
